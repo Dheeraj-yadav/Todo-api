afa27e28ff72b5efebc3a456a2bf61f188baf1a2
divas yadav <dheerajganwal1997@gmail.com>
2018-07-12 01:00:28 -0700

    added code

--- server/server.js
+++ server/server.js
@@ -16,10 +16,14 @@ var{Users} = require('./models/users');
 var app = express();
 app.use(bodyParser.json());
 
+//giving parameters for the todo
+
 app.post('/todos',(req,res) => {
 var todo = new Todo({
   text: req.body.text
 });
+
+//saving the todo we made
 
 todo.save().then((docs) => {
   res.send(docs);
@@ -28,6 +32,14 @@ todo.save().then((docs) => {
 });
 });
 
+//for returning all the todos
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  },(e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(3000, () => {
   console.log('started on port 3000');
